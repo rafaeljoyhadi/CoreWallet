9c15a0f3b814acdd4b6d39562aa1fdfeb89f9dc3
Irvandmw <irvandmw@gmail.com>
2025-04-28 06:59:43 +0700

    Changes on core_savings.xml, core_budget.xml, and core_budget_form.

--- app/src/main/java/com/example/corewallet/CoreSavings.kt
+++ app/src/main/java/com/example/corewallet/CoreSavings.kt
@@ -40,14 +40,11 @@ object RetrofitInstance {
     }
 }
 
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class CoreSavings : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.core_savings)
         window.statusBarColor = Color.parseColor("#0d5892")
 
